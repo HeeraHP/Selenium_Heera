d48c94f05d82832a3a5ac25be45d08f09896ea01
AnjaliTestYantra <anjali.paleth@gmail.com>
2025-02-21 20:06:46 +0530

    Anjali's First commit

---
+++ CRM_GitHub_Heera/src/test/java/anjali/Practice.java
@@ -0,0 +1,12 @@
+package anjali;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice {
+	@Test
+	public void samplePractice() {
+		Reporter.log("Anjali's TS", true);
+	}
+
+}
